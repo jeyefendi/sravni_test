4c8324692653392c3ad21281133ef48e8db896ed
Jeyhun Farajov <97691925+jeyefendi@users.noreply.github.com>
2022-11-03 21:58:59 +0300

    children is not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Card from "./components/Card";
 
 const App = () => {
   return (
     <div>
-
+      <Card width="200px" height="200px">
+        <button>Button</button>
+      </Card>
     </div>
   );
 };
