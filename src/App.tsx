c99e229b89fe7a50a6efb68aec64f7a87dc4dfa9
Jeyhun Farajov <97691925+jeyefendi@users.noreply.github.com>
2022-11-19 02:08:10 +0300

    useEffect axios GET typization

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import Card, {CardVariant} from "./components/Card";
+import UserList from "./components/UserList";
+import { IUser } from "./types/types";
+import axios from "axios";
 
 const App = () => {
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+      setUsers(response.data)
+    } catch (e) {
+      alert(e)
+    }
+  }
+
   return (
     <div>
       <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width="200px" height="200px">
         <button>Button</button>
       </Card>
+      <UserList users={users}/>
     </div> 
   );
 };
